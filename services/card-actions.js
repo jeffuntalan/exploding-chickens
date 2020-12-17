63880361588d050f2cd238d710704ad533618559
rak3rman <rak3rman@gmail.com>
2020-12-16 23:33:10 -0600

    Added header to card-actions

---
+++ services/card-actions.js
@@ -0,0 +1,12 @@
+/*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+Filename : exploding-chickens/services/card-actions.js
+Desc     : all actions and helper functions
+           related to card interaction
+Author(s): RAk3rman
+\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
+
+//Packages
+let game = require('../models/game.js');
+const dataStore = require('data-store');
+const config_storage = new dataStore({path: './config/config.json'});
+let verbose_debug_mode = config_storage.get('verbose_debug_mode');
