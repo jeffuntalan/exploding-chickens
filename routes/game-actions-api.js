0e0e27f73aaf258dff96fe9f160142af93762e4a
rak3rman <rak3rman@gmail.com>
2020-12-22 17:10:07 -0600

    API example

--- routes/game-actions-api.js
+++ routes/game-actions-api.js
@@ -6,12 +6,30 @@ Author(s): RAk3rman
 
 //Export to app.js file
 module.exports = function (app) {
+    //Packages
+    let game = require('../models/game.js');
+    const dataStore = require('data-store');
+    const config_storage = new dataStore({path: './config/config.json'});
+    let verbose_debug_mode = config_storage.get('verbose_debug_mode');
+
     //Services
     let card_actions = require('../services/card-actions.js');
     let game_actions = require('../services/game-actions.js');
     let player_handler = require('../services/player-handler.js');
 
     //Example API route
-    // app.route('/api/example')
-    //     .post(game_actions.create_game());
+    app.route('/api/game/actions/create')
+        .post(
+            function (req, res) {
+                game.find({bib_number: req.query.bib_number}, function (err, details) {
+                    if (err) {
+                        console.log("ENTRY Resolver: Retrieve failed: " + err);
+                        res.send(err);
+                    } else {
+                        console.log("ENTRY Resolver: Entry Sent: " + JSON.stringify(details))
+                    }
+                    res.json(details);
+                });
+            }
+        );
 };
