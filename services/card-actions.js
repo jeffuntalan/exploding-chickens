4319395900eefecc2999c96dd532c7aef548f507
rak3rman <rak3rman@gmail.com>
2020-12-22 19:15:46 -0600

    Fixed attack function

--- services/card-actions.js
+++ services/card-actions.js
@@ -102,75 +102,51 @@ exports.shuffle_draw_deck = async function (game_id, card_id) {
     });
 }
 
-// Name : card_actions.attack_pre(game_id)
-// Desc : forces the next player in turn order to take 2 consecutive turns by changing seat_playing
-// Author(s) : SengdowJones
-exports.attack_pre = async function (game_id) {
+// Name : card_actions.attack(game_id, card_id)
+// Desc : forces the next player in turn order to take 2 consecutive turns
+// Author(s) : RAk3rman
+exports.attack = async function (game_id, card_id) {
+    //Get game details
+    let game_details = await game_actions.game_details(game_id);
+    //Check if we are going forward or backward
+    if (game_details.turn_direction === "forward") {
+        if (!(game_details.players.length <= game_details.seat_playing + 1)) { //Player seat advances by one
+            game_details.seat_playing++;
+        } else {
+            game_details.seat_playing = 0;
+        }
+    } else if (game_details.turn_direction === "backward") {
+        if (!(game_details.seat_playing - 1 < 0)) { //Player seat decreases by one
+            game_details.seat_playing--;
+        } else {
+            game_details.seat_playing = game_details.players.length - 1;
+        }
+    }
+    //Check how many turns we have left
+    if (game_details.turns_remaining <= 1) { //Only one turn left, equal to two turns
+        //Make sure the number of turns remaining is not 0
+        game_details.turns_remaining = 2;
+    } else { //Multiple turns left, turns_remaining
+        game_details.turns_remaining += 2;
+    }
+    //Find next player's id
+    let next_player_id = "";
+    for (let i = 0; i <= game_details.players.length - 1; i++) {
+        if (game_details.players[i].seat === game_details.seat_playing) {
+            next_player_id = game_details.players[i]._id;
+            break;
+        }
+    }
+    //Create new promise
     return await new Promise((resolve, reject) => {
-        game.findById({ _id: game_id }, function (err, found_game) {
+        //Save updated game
+        game_details.save({}, function (err) {
             if (err) {
                 reject(err);
             } else {
-                //Forces next player to take a turn
-                let current = found_game.seat_playing;
-                if (found_game.players.length < found_game.seat_playing + 1) {
-                    let turn = found_game.seat_playing + 1 - found_game.players.length;
-                    if (turn === 0) {
-                        game.findOneAndUpdate({_id: game_id, "seat_playing": found_game.seat_playing},
-                            {"$set": {"seat.$.playing": 0}}, function (err) {
-                                if (err) {
-                                    reject(err);
-                                } else {
-                                    //Resolve promise when the last player has been updated
-                                    if (current !== found_game.seat_playing) {
-                                        resolve(found_game.players.length);
-                                    }
-                                }
-
-                            });
-                    }
-                } else {
-                    game.findOneAndUpdate({_id: game_id, "seat_playing": found_game.seat_playing},
-                        {"$set": {"seat.$.playing": found_game.seat_playing += 1}}, function (err) {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                //Resolve promise when the last player has been updated
-                                if (current !== found_game.seat_playing) {
-                                    resolve(found_game.players.length);
-                                }
-                            }
-                        });
-                }
+                resolve(next_player_id);
             }
         });
-    });
-}
-
-// Name : card_actions.attack_post(game_id)
-// Desc : forces the next player in turn order to take 2 consecutive turns by keeping the same seat_playing
-// Author(s) : SengdowJones
-exports.attack_post = async function (game_id, card_id) {
-    return await new Promise((resolve, reject) => {
-        game.findById({ _id: game_id }, function (err, found_game) {
-            if (err) {
-                reject(err);
-            } else {
-                //Updates seat_playing to same seat_playing
-                    game.findOneAndUpdate({_id: game_id, "seat_playing": found_game.seat_playing},
-                        {"$set": {"seat.$.playing": found_game.seat_playing}}, function (err) {
-                            if (err) {
-                                reject(err);
-                             } else {
-                                //Resolve promise when the last player has been updated
-                                    resolve(found_game.players.length);
-                            }
-                        });
-
-            }
-        });
-        //Move card to discard pile
-        game_actions.discard_card(game_id, card_id);
     });
 }
 
@@ -197,22 +173,21 @@ exports.drawfromthebottom = async function (game_id, card_id) {
 // Author(s) : SengdowJones
 exports.see_the_future = async function (game_id) {
     //Get game details
-
     let game_details = await game_actions.game_details(game_id);
     //Loop through each card to create array
     let bucket = [];
     let bucket_length = 0;
     while (bucket_length < 3) {
         for (let i=0;i<=game_details.cards.length-1;i++) {
-        //Check to see if card in draw deck
-        if (game_details.cards[i].assignment === "draw_deck") {
-            bucket.push(game_details.cards[i]);
-            bucket_length++;
-        } else {
+            //Check to see if card in draw deck
+            if (game_details.cards[i].assignment === "draw_deck") {
+                bucket.push(game_details.cards[i]);
+                bucket_length++;
+            } else {
+
+            }
         }
     }
-    }
-
     //Create new promise
     return await new Promise((resolve, reject) => {
         game.findById({_id: game_id}, function (err, found_game) {
