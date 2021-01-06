4eed366cde81fede66472a27d325329da2f14200
Sengdao <sengdao.inthavong3@gmail.com>
2021-01-05 20:39:17 -0600

    Modal regular expression with form on home.hbs

--- public/js/home.js
+++ public/js/home.js
@@ -8,38 +8,39 @@ Author(s): RAk3rman
 //Open sample modal
 function sample_modal() {
     Swal.fire({
-        html: "<h2 class=\"mt-3 py-50 text-center font-semibold text-3xl lg:text-4xl text-gray-800\">\n" +
-            "                    Join A Lobby\n" +
-            "                </h2>\n" +
-            "\n" +
-            "                <form class=\"mt-10\" method=\"POST\">\n" +
+        html: "<form class=method=\"POST\">\n" +
             "                    <!-- Email Input -->\n" +
-            "                    <label for=\"code\" class=\"block text-xs font-semibold text-gray-600 uppercase\">Code\n" +
-            "                        (word-word-word)</label>\n" +
-            "                    <input id=\"code\" type=\"text\" name=\"code\" placeholder=\"enter-a-code\" autocomplete=\"text\" class=\"block w-full py-3 px-1 mt-2 \n" +
+            "                    <label for=\"code\" class=\"block mt-3 text-left text-xl font-bold text-gray-600\">Joining Lobby\n" +
+            "                        </label>\n" +
+            "                    <input id=\"code\" type=\"text\" name=\"code\" placeholder=\"enter-a-code\" autocomplete=\"text\" class=\"block w-full py-3 px-1 my-1\n" +
             "                    text-gray-800 appearance-none \n" +
             "                    border-b-2 border-gray-100\n" +
             "                    focus:text-gray-500 focus:outline-none focus:border-gray-200\" required maxlength=\"60\"\n" +
             "                        pattern=\"[a-z]*-[a-z]*-[a-z]*\" />\n" +
-            "\n" +
-            "                    <!-- Auth Buttton -->\n" +
-            "                    <button type=\"submit\" class=\"w-full py-3 mt-10 bg-gray-800 rounded-sm\n" +
-            "                    font-medium text-white uppercase\n" +
-            "                    focus:outline-none hover:bg-gray-700 hover:shadow-none\">\n" +
-            "                        Play\n" +
-            "                    </button>\n" +
-            "                </form>",
+            "\n",
         showCancelButton: true,
         cancelButtonColor: '#374151',
         cancelButtonText: 'Cancel',
         confirmButtonText: 'Confirm'
     }).then((result) => {
         if (result.isConfirmed) {
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
+            //Validate input
+            let code = document.getElementById("code").value;
+            var letters = /^[A-Za-z]/;
+            if (code.match(letters)) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Invalid code!',
+                    footer: '<a href>Make sure your code is in a three-word format with dashes.</a>'
+                })
+            }
         }
     })
 }
