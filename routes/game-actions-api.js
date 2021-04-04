0c3cff8745bcd58377573fd2743671a88542043b
rak3rman <rak3rman@gmail.com>
2021-04-04 17:37:57 -0500

    Advanced debug outputs

--- routes/game-actions-api.js
+++ routes/game-actions-api.js
@@ -4,9 +4,9 @@ Desc     : all routes related to game actions
 Author(s): RAk3rman
 \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
 
-//Export to app.js file
+// Export to app.js file
 module.exports = function (fastify) {
-    //Packages
+    // Packages
     let game = require('../models/game.js');
     const dataStore = require('data-store');
     const config_storage = new dataStore({path: './config/config.json'});
@@ -16,12 +16,12 @@ module.exports = function (fastify) {
     const spinner = ora('');
     const wipe = chalk.white;
 
-    //Services
+    // Services
     let card_actions = require('../services/card-actions.js');
     let game_actions = require('../services/game-actions.js');
     let player_actions = require('../services/player-actions.js');
 
-    //Create game route, expecting a player nickname
+    // Create game route, expecting a player nickname
     fastify.post('/game/create', {
         config: {
             rateLimit: {
@@ -30,19 +30,19 @@ module.exports = function (fastify) {
             }
         }
     }, async function (req, reply) {
-        //Create sample game
-        spinner.info(wipe(`${chalk.bold.red('API')}:    ${chalk.dim.cyan('create-game     ')} Received request to create new game`));
+        // Create sample game
+        spinner.start(wipe(`${chalk.bold.red('API')}:    ${chalk.dim.cyan('create-game     ')} Received request to create new game`));
         let game_details = await game_actions.create_game().catch(e => {failed_step(e, reply)});
         spinner.succeed(wipe(`${chalk.bold.red('API')}:    ${chalk.dim.cyan('create-game     ')} ${chalk.dim.yellow(game_details["slug"])} Created new game`));
         let game_id = game_details["_id"];
-        //Import cards
+        // Import cards
         let card_count = await game_actions.import_cards(game_id, '../packs/base.json').catch(e => {failed_step(e, reply)});
         spinner.succeed(wipe(`${chalk.bold.red('API')}:    ${chalk.dim.cyan('create-game     ')} ${chalk.dim.yellow(game_details["slug"])} Imported ` + chalk.bold(card_count) + ` cards from base.json`));
-        //Redirect to game url
+        // Redirect to game url
         reply.redirect("/game/" + game_details["slug"]);
     })
 
-    //Failed step in api
+    // Failed step in api
     function failed_step (desc, reply) {
         spinner.fail(wipe(`${chalk.bold.red('API')}: ${chalk.red('FAIL')} Failed previous step with error message: "` + desc + `"`));
         reply.code(500);
